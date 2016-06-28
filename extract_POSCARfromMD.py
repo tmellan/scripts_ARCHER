f8c0154c4a8d7ac6dd02183773cdddd061492fbe
Dr Thomas Mellan <tmellan@ucl.ac.uk>
2016-06-28 21:23:22 +0100

    python script to extract POSCAR from MD added

--- extract_POSCARfromMD.py
+++ extract_POSCARfromMD.py
@@ -2,7 +2,6 @@
 from pymatgen.io.vaspio import Vasprun
 vr=Vasprun('vasprun.xml')
 allposcar=vr.structures
-dict={"POSCAR"}
 for i, item in enumerate(allposcar):
     print("POSCAR"+str(i))
     item.to(filename="POSCAR"+str(i))
